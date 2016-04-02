090e193b7351446586b71f2a0e33dbca7befa62a
FranchiniMae <fmae.mercado@gmail.com>
2016-04-01 19:44:54 -0700

    Adds controllers

---
+++ api/controllers/goals.js
@@ -0,0 +1,19 @@
+var Goal = require('../models/Goal');
+
+var goalsController = {
+  getAll: function(req, res) {
+  
+    // res.send("successfully hitting get api")
+  },
+  createGoal: function(req, res) {
+  
+  },
+  removeGoal: function(req, res) {
+
+  },
+  updateGoal: function(req, res) {
+
+  }
+};
+
+module.exports = goalsController;
---
+++ api/controllers/tasks.js
@@ -0,0 +1,19 @@
+var Task = require('../models/Task');
+
+var tasksController = {
+  getAll: function(req, res) {
+  
+    // res.send("successfully hitting get api")
+  },
+  createTask: function(req, res) {
+  
+  },
+  removeTask: function(req, res) {
+
+  },
+  updateTask: function(req, res) {
+
+  }
+};
+
+module.exports = tasksController;
